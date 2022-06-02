f7374fc35298d48ee00223b7f4614ebd65473fd6
Rafee Jenkins <rafeejenkins@gmail.com>
2022-06-02 20:57:22 +0100

    Updates

---
+++ precache-manifest.9fe39505545d264b5d4c29317bab84fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09cd758b37dd5602e5df",
+    "url": "/static/css/main.7b305a6e.chunk.css"
+  },
+  {
+    "revision": "09cd758b37dd5602e5df",
+    "url": "/static/js/main.09cd758b.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "eba6ceb283f14a8319df",
+    "url": "/static/js/2.eba6ceb2.chunk.js"
+  },
+  {
+    "revision": "a850c70773011bd5f111",
+    "url": "/static/js/runtime~main.a850c707.js"
+  },
+  {
+    "revision": "c2b7234328245c3e0e6500d2722be432",
+    "url": "/index.html"
+  }
+];
