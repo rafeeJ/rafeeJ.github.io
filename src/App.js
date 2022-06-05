4a415f859617f0265b979945d5069501161ae601
Rafee Jenkins <rafeejenkins@gmail.com>
2022-06-05 13:07:07 +0100

    Minor refactor

--- src/App.js
+++ src/App.js
@@ -70,61 +70,61 @@ function App() {
   }
 
   const checkPath = (puzzle, i, j, visited) => {
-        // Checking the boundaries, walls and whether the cell is unvisited
-        if (
-          isSafe(i, j, puzzle)
-          && puzzle[i][j] !== 0
-          && !visited[i][j]) {
-          // Make the cell visited
-          visited[i][j] = true;
+    // Checking the boundaries, walls and whether the cell is unvisited
+    if (
+      isSafe(i, j, puzzle)
+      && puzzle[i][j] !== 0
+      && !visited[i][j]) {
+      // Make the cell visited
+      visited[i][j] = true;
 
-          // if the cell is the required
-          // destination then return true
-          if (puzzle[i][j] === puzzle[i][j].toUpperCase() && i!==0 && j!==0)
-              return true;
+      // if the cell is the required
+      // destination then return true
+      if (puzzle[i][j] === puzzle[i][j].toUpperCase() && i !== 0 && j !== 0)
+        return true;
 
-          // traverse up
-          let up = checkPath(
-              puzzle, i - 1,
-              j, visited);
+      // traverse up
+      let up = checkPath(
+        puzzle, i - 1,
+        j, visited);
 
-          // if path is found in up
-          // direction return true
-          if (up)
-              return true;
+      // if path is found in up
+      // direction return true
+      if (up)
+        return true;
 
-          // traverse left
-          let left
-              = checkPath(
-                  puzzle, i, j - 1, visited);
+      // traverse left
+      let left
+        = checkPath(
+          puzzle, i, j - 1, visited);
 
-          // if path is found in left
-          // direction return true
-          if (left)
-              return true;
+      // if path is found in left
+      // direction return true
+      if (left)
+        return true;
 
-          // traverse down
-          let down = checkPath(
-              puzzle, i + 1, j, visited);
+      // traverse down
+      let down = checkPath(
+        puzzle, i + 1, j, visited);
 
-          // if path is found in down
-          // direction return true
-          if (down)
-              return true;
+      // if path is found in down
+      // direction return true
+      if (down)
+        return true;
 
-          // traverse right
-          let right
-              = checkPath(
-                  puzzle, i, j + 1,
-                  visited);
+      // traverse right
+      let right
+        = checkPath(
+          puzzle, i, j + 1,
+          visited);
 
-          // if path is found in right
-          // direction return true
-          if (right)
-              return true;
-      }
-      // no path has been found
-      return false;
+      // if path is found in right
+      // direction return true
+      if (right)
+        return true;
+    }
+    // no path has been found
+    return false;
   }
 
   const checkForPath = (puzzle) => {
@@ -149,14 +149,14 @@ function App() {
   }
 
   const checkAdj = (i, j, puzzle) => {
-    
-    let right = isSafe(i, j+1, puzzle) ? puzzle[i][j+1] : null
-    let left = isSafe(i, j-1, puzzle) ? puzzle[i][j-1] : null
-    
-    let down = isSafe(i+1, j, puzzle) ? puzzle[i+1][j] : null
-    let up = isSafe(i-1, j, puzzle) ? puzzle[i-1][j] : null
 
-  return {down: down, up: up, left:left, right: right}
+    let right = isSafe(i, j + 1, puzzle) ? puzzle[i][j + 1] : null
+    let left = isSafe(i, j - 1, puzzle) ? puzzle[i][j - 1] : null
+
+    let down = isSafe(i + 1, j, puzzle) ? puzzle[i + 1][j] : null
+    let up = isSafe(i - 1, j, puzzle) ? puzzle[i - 1][j] : null
+
+    return { down: down, up: up, left: left, right: right }
   }
 
   const checkForOverrun = (puzzle) => {
@@ -172,17 +172,15 @@ function App() {
           let dirs = checkAdj(i, j, puzzle)
 
           var numOfAdjacentLetters = _.values(dirs).filter(x => typeof x === "string").length
-          
+
           if (numOfAdjacentLetters < 2 && puzzle[i][j] !== puzzle[i][j].toUpperCase()) {
-            // console.log(numOfAdjacentLetters);
-            // console.log(`row: ${i}, col: ${j}, val: ${puzzle[i][j]}`)
             overrun = true
           }
 
           //console.log(dirs);
         }
       }
-    } 
+    }
     return overrun
   }
 
@@ -193,20 +191,19 @@ function App() {
     // Check if its even valid
     var truthPath = checkForPath(saniPuzzle)
     var overrun = checkForOverrun(saniPuzzle)
-    
+
     if (truthPath === null || overrun) {
       alert('Invalid')
     } else {
-      //console.log(truthPath);
-      var startCell = {x: 0, y:0}
+      var startCell = { x: 0, y: 0 }
       var words = []
-      
-      while (startCell.x <= 5 && startCell.y <= 5) {
-        
-        if (startCell.x === 5 && startCell.y === 5) {
+
+      while (startCell.x <= saniPuzzle.length - 1 && saniPuzzle.length - 1) {
+
+        if (startCell.x === saniPuzzle.length - 1 && startCell.y === saniPuzzle.length - 1) {
           break
         }
-    
+
         let down = lookDown(startCell, saniPuzzle)
         let right = lookRight(startCell, saniPuzzle)
 
@@ -224,16 +221,16 @@ function App() {
     const valid = words.every((val) => {
       return WORDS.includes(val.toLowerCase())
     })
+    return valid
+  }
 
+  const handleSubmit = () => {
+    const valid = checkPuzzle(puzzle)
     if (valid) {
       alert("Looks good - well done")
     } else {
       alert("Something looks goofy")
     }
-  }
-
-  const handleSubmit = () => {
-    checkPuzzle(puzzle)
   }
 
   return (
